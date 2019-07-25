fd6c2fcaeccbaffb91b2a0b7c9ce42a166f3dd27
Chris Pelzer <chrispelzer@gmail.com>
2019-07-25 11:45:09 -0400

    Use the default field wrapper which provides the benefit of error and the help text as well and others that were included with this field.

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,25 +1,13 @@
 <template>
-    <field-wrapper>
-        <div class="w-1/5 px-8 py-6">
-            <slot>
-                <form-label :for="field.name">
-                    {{ field.name }}
-                </form-label>
-            </slot>
-        </div>
-
-        <div class="w-4/5 px-8 py-6">
+    <default-field :field="field" :errors="errors" :full-width-content="true">
+        <template slot="field">
             <vue-ckeditor
                 :id="field.name"
                 v-model="value"
                 :config="config"
             />
-
-            <p v-if="hasError" class="my-2 text-danger">
-                {{ firstError }}
-            </p>
-        </div>
-    </field-wrapper>
+        </template>
+    </default-field>
 </template>
 
 
